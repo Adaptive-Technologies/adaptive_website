362b9603b553b74967eeea31f903732f1e25838b
kim_haaga <kim.haaga@live.se>
2021-09-24 15:25:04 +0200

    Add style to grid

--- src/components/our_clients/ClientsSection.jsx
+++ src/components/our_clients/ClientsSection.jsx
@@ -10,40 +10,47 @@ import KCSC from '../../assets/images/Symbol_Logo_Colour.png'
 const ClientsSection = () => {
   const classes = useStyles()
   return (
-    <Box>
-      <CardMedia component='img' image={background} alt='' />
-      <Typography className={classes.header} data-cy='header' variant='h2'>
-        {'< clients that are adapting />'}
-      </Typography>
-      <Grid className={classes.grid} container>
-        <Grid item xs={4}>
-          <CardMedia
-            className={classes.logo}
-            data-cy='logo-1'
-            component='img'
-            image={FlexCoast}
-            alt='Flex Coast logo'
-          />
+    <Box className={classes.clientsSection}>
+      <CardMedia
+        className={classes.background}
+        component='img'
+        image={background}
+        alt=''
+      />
+      <Box className={classes.contentBox}>
+        <Typography data-cy='header' variant='h2'>
+          {'< clients that are adapting />'}
+        </Typography>
+        <Grid className={classes.grid} container>
+          <Grid className={classes.gridItem} item xs={4}>
+            <CardMedia
+              className={classes.logo}
+              data-cy='logo-1'
+              component='img'
+              image={FlexCoast}
+              alt='Flex Coast logo'
+            />
+          </Grid>
+          <Grid className={classes.gridItem} item xs={4}>
+            <CardMedia
+              className={classes.logo}
+              data-cy='logo-2'
+              component='img'
+              image={Timotuz}
+              alt='Timotuz logo'
+            />
+          </Grid>
+          <Grid className={classes.gridItem} item xs={4}>
+            <CardMedia
+              className={classes.logo}
+              data-cy='logo-3'
+              component='img'
+              image={KCSC}
+              alt='KCSC logo'
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <CardMedia
-            className={classes.logo}
-            data-cy='logo-2'
-            component='img'
-            image={Timotuz}
-            alt='Timotuz logo'
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <CardMedia
-            className={classes.logo}
-            data-cy='logo-3'
-            component='img'
-            image={KCSC}
-            alt='KCSC logo'
-          />
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   )
 }
--- src/theme/views/clients.theme.js
+++ src/theme/views/clients.theme.js
@@ -1,17 +1,53 @@
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
-  header: {
-
+  clientsSection: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '500px',
+  },
+  background: {
+    height: '70vh',
+    minHeight: '500px',
+    [theme.breakpoints.up('lg')]: {
+      height: '80vh',
+    },
+  },
+  contentBox: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '15%',
+    [theme.breakpoints.up('sm')]: {
+      marginTop: '15%',
+    },
+    [theme.breakpoints.up('md')]: {
+      marginTop: '15%',
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '5%',
+    },
   },
   grid: {
-    display: 'flex',
-    width: '20%',
-    justifyItems: 'center'
+    alignItems: 'center',
+    marginTop: '20%',
+    [theme.breakpoints.up('md')]: {
+      marginTop: '10%',
+    },
+  },
+  gridItem: {
+    paddingBottom: '36px',
   },
   logo: {
-    maxHeight: '100px',
-    maxWidth: '100px',
+    maxHeight: '75px',
+    maxWidth: '75px',
+    margin: 'auto',
+    [theme.breakpoints.up('md')]: {
+      maxHeight: '100px',
+      maxWidth: '100px',
+      margin: 'auto',
+    },
   },
 }))
 
