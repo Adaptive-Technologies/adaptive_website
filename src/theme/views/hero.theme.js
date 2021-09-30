8608a63369501a1f16aca3dba12b470dccf18d80
kim_haaga <kim.haaga@live.se>
2021-09-30 15:18:02 +0200

    update to 20% opacity on hero tint

--- src/theme/views/hero.theme.js
+++ src/theme/views/hero.theme.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     width: '100%',
     height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
   },
   overlayContent: {
     display: 'flex',
